7a29d5ed6e18fbbdf856ae11dfe0643a1f66a0ca
Ashish <ashish_kumar97@edgeverve.com>
2018-01-26 00:42:11 +0530

    Deployment changes for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://system:manager@ds129374.mlab.com:29374/todoapp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(bodyParser.json());
@@ -45,8 +46,8 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
-app.listen(3000, () => {
-    console.log("Server started at 3000");
+app.listen(port, () => {
+    console.log(`Server started at ${port}`);
     
 });
 
