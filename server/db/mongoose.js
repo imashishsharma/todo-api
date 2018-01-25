72c3ee65abf8686aaeb96aabfed960ae06b9341f
Ashish <ashish_kumar97@edgeverve.com>
2018-01-26 01:01:02 +0530

    Added MONGO_URL env variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://system:manager@ds129374.mlab.com:29374/todoapp');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://system:manager@ds129374.mlab.com:29374/todoapp');
 
 module.exports = {mongoose};
