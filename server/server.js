3c49e64d8449e606e1e17b0d84abadac9d0092b2
Ashish <ashish_kumar97@edgeverve.com>
2018-01-26 11:22:23 +0530

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -46,6 +46,20 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send("ID is invalid");
+    };
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send("ID not found");
+        };
+        res.status(200).send(todo);
+    }).catch((e) => res.status(400).send("Some exception occurred"));
+});
+
+
 app.listen(port, () => {
     console.log(`Server started at ${port}`);
     
