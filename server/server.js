9334a390b2f7c2839754378084a57afdecf514a9
Ashish <ashish_kumar97@edgeverve.com>
2018-01-26 12:31:30 +0530

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -59,6 +60,28 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send("Some exception occurred"));
 });
 
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send("ID is invalid");
+    };
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new :true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send("ID not found");
+        };
+        res.send({todo});
+    }).catch((e) => res.status(400).send("Some exception occurred"));
+});
 
 app.listen(port, () => {
     console.log(`Server started at ${port}`);
