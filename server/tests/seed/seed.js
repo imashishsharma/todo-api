001b719274fcacf7104ae1334fe3b0296456d9c6
Ashish <6ashishs@gmail.com>
2018-01-28 16:05:56 +0530

    Made todos route private

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,14 +1,19 @@
 var {ObjectID} = require('mongodb');
 var {Todo} = require('./../../models/todo');
 
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
 const todos = [{
     _id: new ObjectID(),
-    text: "Some seed data test1"
+    text: "Some seed data test1",
+    _creator: userOneId
 }, {
     _id: new ObjectID(),
     text: "Some seed data test2",
     completed: true,
-    completedAt: 345
+    completedAt: 345,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
